cb6b4b08a0855ea60f2048388f6acc26c7283a5f
elmarthg <ejanettygallarde@gmail.com>
2022-04-08 10:39:15 -0700

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -133,4 +133,17 @@ From PercentPopulationVaccinated
 
 
 -- Creating View to store data for later visualizations
+
+
+
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From master..CovidDeaths dea
+Join master..CovidVax vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+
                                                                                                                                                                                               
